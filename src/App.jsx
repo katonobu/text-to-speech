0e939414c45f78b8e099dec66b83d2b134bf0708
Nobuo Kato <katonobu4649@gmail.com>
2025-11-24 13:22:01 +0000

    基本機能動作確認OK

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import TextToSpeech from './TextToSpeech';
+
+const App = () => {
+  const [kaisetsu, setKaisetsu] = useState(null)
+  const [loading, setLoading] = useState(true)  
+  useEffect(() => {
+    fetch("/kaisetsu_tanki.json")
+      .then(response => {
+        if (!response.ok) throw new Error('Failed to fetch metadata')
+        return response.json()
+      })
+      .then(data => {
+        const tracks = []
+        tracks.push([
+          data[0].name,
+          data[0].sentences[0]
+        ].join("\n"))
+        tracks.push(data[1].name)
+        data[1].subsections.forEach(item => {
+          const texts = []
+          texts.push(item.name)
+          item.sentences.forEach(sentence => texts.push(sentence))
+          tracks.push(texts.join("\n"))
+        })
+        tracks.push(data[2].name)
+        data[2].subsections.forEach(item => {
+          const texts =[]
+          texts.push(item.name)
+          item.sentences.forEach(sentence => texts.push(sentence))
+          tracks.push(texts.join("\n"))
+        })
+        setKaisetsu(tracks)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error('Error fetching metadata:', error)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <p className="p-4">読み込み中...</p>
+  if (!kaisetsu) return <p className="p-4">データがありません。</p>
+
+  return (
+    <div>
+      <TextToSpeech tracks={kaisetsu} />
+      <ul>
+        {kaisetsu.map((item,idx) => (<li key={idx}>{item}</li>))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/TextToSpeech.jsx
@@ -0,0 +1,148 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const TextToSpeech = ({ tracks }) => {
+  const [playingStt, setPlayingStt] = useState("IDLE");
+  const [currentTrack, setCurrentTrack] = useState(0);
+  const synth = window.speechSynthesis;
+  const utteranceRef = useRef(null);
+
+  // reloadされるとき、再生を停止させる。
+  useEffect(() => {
+    const handleUnload = () => {
+      window.speechSynthesis.cancel();
+    };
+    window.addEventListener("beforeunload", handleUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, []);
+
+  // 指定トラックの再生を開始する
+  const playTrack = (index, pauseImmediately = false) => {
+    // 指定トラックが最後まで到達してたら停止
+    if (index >= tracks.length) {
+      console.log("All tracks finished");
+      setPlayingStt("IDLE");
+      setCurrentTrack(0);
+      return;
+    }
+
+    // 指定トラック文字列を登録
+    const u = new SpeechSynthesisUtterance(tracks[index]);
+    utteranceRef.current = u;
+
+    // 再生開始時ハンドラ
+    u.onstart = () => {
+      if (pauseImmediately) {
+        synth.pause()
+        console.log(`Pause track ${index}`);
+      } else {
+        setPlayingStt("PLAY");
+        console.log(`Start track ${index}`);
+      }
+    };
+    // 一時停止時ハンドラ
+    u.onpause = () => {
+      setPlayingStt("PAUSE");
+      console.log("Pause")
+    }
+    // 一時停止解除時ハンドラ
+    u.onresume = () => {
+      setPlayingStt("PLAY");
+      console.log("Resume")
+    }
+    // トラック最後まで再生したときハンドラ
+    u.onend = () => {
+      if (currentTrack < tracks.length - 1) {
+        console.log(`Track ${index} ended`);
+        setCurrentTrack(index + 1);
+        playTrack(index + 1); // 次を再生
+      }
+    };
+
+    // 先頭から再生開始
+    synth.speak(u);
+  };
+
+  const play = () => {
+    if (playingStt === "IDLE") {
+      playTrack(currentTrack);
+    }
+  };
+
+  const pause = () => {
+    if (playingStt === "PLAY") {
+      synth.pause();
+    }
+  };
+
+  const resume = () => {
+    if (playingStt === "PAUSE") {
+      synth.resume();
+    }
+  }
+
+  const stop = () => {
+    synth.cancel();
+    console.log("Stop")
+    setPlayingStt("IDLE");
+    setCurrentTrack(0);
+  };
+
+  const handlePlayPause = () => {
+    if (playingStt === "IDLE") {
+      play()
+    } else if (playingStt === "PLAY") {
+      pause()
+    } else if (playingStt === "PAUSE") {
+      resume()
+    } else {
+      console.log(`Unexpected stt ${playingStt}`)
+    }
+  }
+  const playPauseButtonStr = () => {
+    if (playingStt === "IDLE") {
+      return "PLAY"
+    } else if (playingStt === "PLAY") {
+      return "PAUSE"
+    } else if (playingStt === "PAUSE") {
+      return "RESUME"
+    } else {
+      return `Error : ${playingStt}`
+    }
+  }
+  const prevTrack = () => {
+    if (0 < currentTrack) {
+      setCurrentTrack(currentTrack - 1);
+      if (playingStt !== "IDLE") {
+        synth.cancel(); // 再生停止して
+        playTrack(currentTrack - 1, playingStt === "PAUSE"); // 指定トラックを再生or先頭でpause
+      } else {
+        // IDLE時はcurrentTrackの変更だけ
+      }
+    }
+  }
+  const nextTrack = () => {
+    if (currentTrack < tracks.length -1 ) {
+      setCurrentTrack(currentTrack + 1);
+      if (playingStt !== "IDLE") {
+        synth.cancel();
+        playTrack(currentTrack + 1, playingStt === "PAUSE"); // 指定トラックを再生or先頭でpause
+      } else {
+        // IDLE時はcurrentTrackの変更だけ
+      }
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={prevTrack} disabled={0 == currentTrack}>{"<<"}</button>
+      <button onClick={handlePlayPause}>{playPauseButtonStr()}</button>
+      <button onClick={stop} disabled={playingStt === "IDLE"}>STOP</button>
+      <button onClick={nextTrack} disabled={!(currentTrack < tracks.length - 1)}>{">>"}</button>
+      <p>{currentTrack+1}/{tracks.length}</p>
+    </div>
+  );
+};
+
+export default TextToSpeech;
