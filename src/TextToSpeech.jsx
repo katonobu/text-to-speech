11e0e1c49fa0b655314e30273c97aabbe02b968d
Nobuo Kato <katonobu4649@gmail.com>
2025-11-28 16:50:39 +0000

    tailwind適用

--- src/TextToSpeech.jsx
+++ src/TextToSpeech.jsx
@@ -26,17 +26,40 @@ const TextToSpeech = () => {
     playingStt === "IDLE" ? "▶" : playingStt === "PLAY" ? "⏸" : "⏸▶";
 
   return (
-    <div>
-      <h1>クライアント側音声読み上げサンプル</h1>
-      <h2>読み上げ制御コンポーネント</h2>
+    <div className="p-4">
+      <h1
+        className="text-2xl font-bold"
+      >クライアント側音声読み上げサンプル</h1>
+      <h2
+        className="text-lg font-bold"
+      >読み上げ制御コンポーネント</h2>
       <p>TRACK : {currentTrack + 1}/{totalTracks}</p>
-      <button onClick={prevTrack} disabled={currentTrack === 0}>{"⏮"}</button>
-      <button onClick={playPause}>{buttonLabel}</button>
-      <button onClick={stop} disabled={playingStt === "IDLE"}>⏹</button>
-      <button onClick={nextTrack} disabled={currentTrack >= totalTracks - 1}>{"⏭"}</button>
-      <h2>読み上げ対象テキスト</h2>
-      <ul>
-        {texts.map((item,idx) => (<li key={idx}>{item}</li>))}
+      <button 
+        class="border border-gray-500 rounded w-[25px] h-[25px] text-black bg-white"
+        onClick={prevTrack}
+        disabled={currentTrack === 0}
+      >{"⏮"}</button>
+      <button
+        class="border border-gray-500 rounded w-[40px] h-[25px] text-black bg-white"
+        onClick={playPause}
+      >{buttonLabel}</button>
+      <button
+        class="border border-gray-500 rounded w-[25px] h-[25px] text-black bg-white"
+        onClick={stop}
+        disabled={playingStt === "IDLE"}
+      >⏹</button>
+      <button
+        class="border border-gray-500 rounded w-[25px] h-[25px] text-black bg-white"
+        onClick={nextTrack}
+        disabled={currentTrack >= totalTracks - 1}
+      >{"⏭"}</button>
+      <h2
+        className="text-lg font-bold"
+      >読み上げ対象テキスト</h2>
+      <ul class="list-decimal text-wrap px-10">
+        {texts.map((item,idx) => (<li
+          key={idx}
+        >{item}</li>))}
       </ul>
     </div>
   );
